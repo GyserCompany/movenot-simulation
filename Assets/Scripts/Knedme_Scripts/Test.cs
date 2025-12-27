92bea26526ae2b37306224d98eaab38d30f3f31b
knedme <knedme1@gmail.com>
2025-12-27 15:26:48 +0300

    script change

--- Assets/Scripts/Knedme_Scripts/Test.cs
+++ Assets/Scripts/Knedme_Scripts/Test.cs
@@ -1,15 +1,11 @@
-﻿using System;
 using UnityEngine;
 
-public class PROVERKA : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Console.WriteLine("XYZ");
-        Console.WriteLine("PIDOR");
-        //заебал
-        // нет ты
+        
     }
 
     // Update is called once per frame
