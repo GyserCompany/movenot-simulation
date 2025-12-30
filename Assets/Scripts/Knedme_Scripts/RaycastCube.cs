dff1f36c0ad00938c66088a852ef401276b3edb7
knedme <knedme1@gmail.com>
2025-12-30 14:09:04 +0300

    Raycast ver2

--- Assets/Scripts/Knedme_Scripts/RaycastCube.cs
+++ Assets/Scripts/Knedme_Scripts/RaycastCube.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RaycastCube : MonoBehaviour
@@ -7,27 +6,57 @@ public class RaycastCube : MonoBehaviour
     // Параметры отображения лучей
     private const float RayDistance = 10f; // Длинна полёта лучей
     private const float RayWidth = 0.01f; // Диаметр отображаемого луча
-    private const float RayEmitAngle = 15f; // Угол испускания лучей
-    private const int RayCount = 8; // Количество испускаемых лучей
+    private const float RayEmitAngle = 15f; // Максимальный угол испускания лучей
+    private const float RayDeltaAngle = 1f; // Угол, на который постепенно увеличивается угол испускания
+    private const int RayCount = 20; // Максимальное количество лучей в окружности
+
     void Start()
     {
-        // Формируем массив направлений лучей
-        Quaternion[] rayDirs = new Quaternion[RayCount];
-        const float deltaRad = 2 * (float) Math.PI / RayCount;
-        for (int i = 0; i < RayCount; i++)
+        // Создадим массив направлений лучей
+        List<Vector3> rayDirs = new List<Vector3>();
+        Vector3 direction = transform.forward;
+        for (float angle = RayEmitAngle; angle > 0.001f; angle -= RayDeltaAngle)
         {
-            rayDirs[i] = Quaternion.Euler(
-                (float) Math.Cos(deltaRad * i) * RayEmitAngle,
-                (float) Math.Sin(deltaRad * i) * RayEmitAngle,
-                0
+            // Расчитываем количество лучей в окружности для определённого угла
+            int countAtAngle = Mathf.Max(
+                1,
+                Mathf.RoundToInt(
+                    RayCount * (Mathf.Tan(angle * Mathf.Deg2Rad) / Mathf.Tan(RayEmitAngle * Mathf.Deg2Rad))
+                )
             );
+ 
+            // Добавляем новые круговые направления
+            rayDirs.AddRange(CircleDirs(angle, countAtAngle, direction));
         }
 
+        DrawRays(rayDirs.ToArray());
+    }
+
+    // Возвращает массив круговых направлений для определённого угла
+    Vector3[] CircleDirs(float emitAngle, int rayCount, Vector3 dir)
+    {
+        Vector3[] dirs = new Vector3[rayCount];
+        float deltaRad = 2 * Mathf.PI / rayCount;
+        for (int i = 0; i < rayCount; i++)
+        {
+            Quaternion angle = Quaternion.Euler(
+                Mathf.Cos(deltaRad * i) * emitAngle,
+                Mathf.Sin(deltaRad * i) * emitAngle,
+                0f
+            );
+            dirs[i] = angle * dir;
+        }
+        return dirs;
+    }
+
+    // Отрисовывает лучи по массиву направлений, переданному в функцию 
+    void DrawRays(Vector3[] rayDirs)
+    {
         // Перебираем все направления
         for (int i = 0; i < rayDirs.Length; i++)
         {
             // Создаём луч
-            Ray ray = new Ray(transform.position, rayDirs[i] * transform.forward);
+            Ray ray = new Ray(transform.position, rayDirs[i]);
             RaycastHit hit;
 
             // Создаём дочерний объект
@@ -56,6 +85,4 @@ public class RaycastCube : MonoBehaviour
             }
         }
     }
-
-    void Update() {}
 }
